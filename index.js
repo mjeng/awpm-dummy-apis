3c037643f4ecf8afb79535808afd095a97c9a182
Matthew Jeng <mjeng@berkeley.edu>
2019-03-20 13:40:55 -0700

    updated

--- index.js
+++ index.js
@@ -8,6 +8,8 @@ exports.dummySiteSpeed = (req, res) => {
   // let message = req.query.message || req.body.message || 'Hello World!';
   siteSpeed = Math.floor(Math.random() * 1000 + 200);
   cacheHitRate = Math.floor(Math.random() * 60 + 20) / 100;
+  res.set('Access-Control-Allow-Origin', "*")
+  res.set('Access-Control-Allow-Methods', 'GET, POST')
   res.status(200).json({
     "siteSpeed": siteSpeed,
     "cacheHitRate": cacheHitRate
@@ -17,6 +19,9 @@ exports.dummySiteSpeed = (req, res) => {
 const request = require('request');
 
 exports.dummyUptime = (req, res) => {
+  res.set("Content-Type", "application/json")
+  res.set('Access-Control-Allow-Origin', "*")
+  res.set('Access-Control-Allow-Methods', 'GET, POST')
   if (req.method === "GET") {
     time = Math.floor(Math.random() * 5000000 + 100000);
     errorRate = Math.floor(Math.random() * 80) / 10000;
@@ -25,7 +30,9 @@ exports.dummyUptime = (req, res) => {
       "errorRate": errorRate
     });
   } else if (req.method === "POST") {
-    request(req.param("endpoint"));
-    res.status(200).send("sent\n");
+    request('http://ce912ffb.ngrok.io/downtime', (err, response, body) => {
+        res.status(200).send()
+    });
+    
   }
 }
